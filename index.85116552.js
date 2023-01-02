3d3ac6c2113914de7e376ec0c7483368b61ba463
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-01-02 08:38:58 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/Restaurant-Mimino@887a0abba97b1fc3f8ce97f57e87eec55b25eff6 🚀

---
+++ index.85116552.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-mini-mod",{slidesPerView:"auto",loop:!0,navigation:{nextEl:".swiper-button-next-mini-mod",prevEl:".swiper-button-prev-mini-mod"},breakpoints:{320:{spaceBetween:20},1024:{spaceBetween:34},1280:{spaceBetween:86}}});
+//# sourceMappingURL=index.85116552.js.map
---
+++ restaurant.781cb777.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-mini",{spaceBetween:25,loop:!0,navigation:{nextEl:".swiper-button-next-mini",prevEl:".swiper-button-prev-mini"},breakpoints:{320:{slidesPerView:1.3,spaceBetween:25},1024:{slidesPerView:2,spaceBetween:40}}});
+//# sourceMappingURL=restaurant.781cb777.js.map
